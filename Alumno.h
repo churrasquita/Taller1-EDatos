e2e52baa0e44c889450509fef611c097378b5e7b
Catalina <catalina.galleguillos@alumnos.ucn.cl>
2025-09-09 00:00:37 -0300

    Add files via upload

---
+++ Alumno.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+
+class Alumno{
+    private:
+        int id; // identificador único del alumno
+        std::string nombre;
+        std::string apellido;
+        std::string carrera;
+        std::string ingreso;
+    public:
+        Alumno(int id, std::string nombre, std:: string apellido, std:: string carrera, std:: string ingreso);
+        int getId();
+        std::string getNombre();
+        std::string getApellido();
+        std::string getCarrera();
+        std::string getIngreso();
+        void toString();
+        ~Alumno(); //destructor
+};
