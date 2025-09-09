a6617ecc9c96d0d709cedfc787544c7c368fb88f
Catalina <catalina.galleguillos@alumnos.ucn.cl>
2025-09-08 23:58:08 -0300

    Clase alumno

---
+++ Alumno.cpp
@@ -0,0 +1,16 @@
+#include "Alumno.h"
+#include <iostream>
+
+using namespace std;
+
+Alumno::Alumno(string id, string nombre, string apellido, string carrera, string ingreso){
+    this->id=id;
+    this->nombre=nombre;
+    this->apellido=apellido;
+    this->carrera=carrera;
+    this->ingreso=ingreso;
+}
+
+void Alumno::toString(){
+    std::cout<<"id: "<<id<<" /Nombre: "<<nombre<<" /Apellido: "<<maxEstudiantes<<" /Carrera:"<< carrera<<"/Año ingreso: "<<ingreso<<std::endl;
+}
